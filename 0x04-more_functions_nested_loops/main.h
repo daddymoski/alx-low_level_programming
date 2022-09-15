cacdb5da1ddf78ec513704f5ffb001c5fb3c6b26
daddymoski <abdulhamid.abubakar@hotmail.com>
2022-09-15 12:45:52 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+
+int_putchar(char c);
+int_isupper(int c);
+int_isdigit(int c);
+int mul(int a,int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void pint_more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_numbers(int n);
+
+#endif
+
+
+	
+
