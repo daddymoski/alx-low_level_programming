7b0c475999719d650a8d182c4d9f47f10a052b87
daddymoski <abdulhamid.abubakar@hotmail.com>
2022-09-15 12:52:08 -0700

    ll

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int_isdigit(int c);
 int mul(int a,int b);
 void print_numbers(void);
 void print_most_numbers(void);
-void pint_more_numbers(void);
+void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
